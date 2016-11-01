480d3afd1be7a1a1c566bf8d09c712e745109136
eye-jacked <stuglik.r@gmail.com>
2016-11-01 19:39:41 +0100

    database connections initialized

---
+++ api/source/dbinit.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rob
+ * Date: 01.11.16
+ * Time: 19:34
+ */
---
+++ source/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+$configuration = array(
+    'db_dsn'  => 'mysql:host=localhost;dbname=PrintPlaza',
+    'db_user' => 'root',
+    'db_pass' => 'CodersLab',
+);
+
+require_once __DIR__.'/lib/Service/Container.php';
+
